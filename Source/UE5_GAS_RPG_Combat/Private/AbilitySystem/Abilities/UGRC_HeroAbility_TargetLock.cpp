67d0d63d6a50900616dd78c8c9d7e44bdbc2409e
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-04-04 20:48:01 +0530

    Setting Up Player Target Lock

---
+++ Source/UE5_GAS_RPG_Combat/Private/AbilitySystem/Abilities/UGRC_HeroAbility_TargetLock.cpp
@@ -0,0 +1,15 @@
+#include "AbilitySystem/Abilities/UGRC_HeroAbility_TargetLock.h"
+
+void UUGRC_HeroAbility_TargetLock::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
+
+void UUGRC_HeroAbility_TargetLock::EndAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
