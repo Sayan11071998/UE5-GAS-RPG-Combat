30861f8624d79106de6d45f371eda694f0699d2e
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-04-13 23:24:56 +0530

    Retrieve Game Level Tag Logic Implemented

--- Source/UE5_GAS_RPG_Combat/Private/UGRC_GameInstance.cpp
+++ Source/UE5_GAS_RPG_Combat/Private/UGRC_GameInstance.cpp
@@ -1,1 +1,16 @@
 #include "UGRC_GameInstance.h"
+
+TSoftObjectPtr<UWorld> UUGRC_GameInstance::GetGameLevelByTag(FGameplayTag InTag)
+{
+	for (const FUGRC_GameLevelSet& GameLevelSet : GameLevelSets)
+	{
+		if (!GameLevelSet.IsValid()) continue;
+		
+		if (GameLevelSet.LevelTag == InTag)
+		{
+			return GameLevelSet.Level;
+		}
+	}
+	
+	return TSoftObjectPtr<UWorld>();
+}
--- Source/UE5_GAS_RPG_Combat/Public/UGRC_GameInstance.h
+++ Source/UE5_GAS_RPG_Combat/Public/UGRC_GameInstance.h
@@ -1,11 +1,37 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "Engine/GameInstance.h"
 #include "UGRC_GameInstance.generated.h"
+
+USTRUCT(BlueprintType)
+struct FUGRC_GameLevelSet
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly, meta = (Categories = "GameData.Level"))
+	FGameplayTag LevelTag;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TSoftObjectPtr<UWorld> Level;
+	
+	bool IsValid() const
+	{
+		return LevelTag.IsValid() && !Level.IsNull();
+	}
+};
 
 UCLASS()
 class UE5_GAS_RPG_COMBAT_API UUGRC_GameInstance : public UGameInstance
 {
 	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, meta = (GameplayTahFilter = "GameData.Level"))
+	TSoftObjectPtr<UWorld> GetGameLevelByTag(FGameplayTag InTag);
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FUGRC_GameLevelSet> GameLevelSets;
 };
